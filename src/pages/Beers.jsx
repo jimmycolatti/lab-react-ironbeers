60efc98539689ae75153b0400b54f6dda05727c4
Jimmy Colatti <jimmyrcolatti@gmail.com>
2022-05-21 17:30:25 -0400

    complete

--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -5,12 +5,17 @@ import Header from "../components/Header"
 
 const Beers = () => {
   const [beersList, setBeersList] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
 
   const getBeers = async () => {
     const { data } = await axios.get(
       `https://ih-beers-api2.herokuapp.com/beers`
     )
     setBeersList(() => data)
+  }
+
+  const changeHandler = (e) => {
+    setSearchTerm(e.target.value)
   }
 
   useEffect(() => {
@@ -24,32 +29,46 @@ const Beers = () => {
   return (
     <div>
       <Header />
+      <div>
+        <p>
+          Search:{" "}
+          <input type="search" value={searchTerm} onChange={changeHandler} />
+        </p>
+      </div>
 
       <div>
-        {beersList.map((beer) => {
-          return (
-            <div key={beer._id}>
-              <img
-                src={beer.image_url}
-                style={{ width: "50px" }}
-                alt={beer.name}
-              />
-              <Link
-                to={`/beers/${beer._id}`}
-                style={{ textDecoration: "none" }}
-              >
-                <h2>{beer.name}</h2>
-              </Link>
-              <br />
-              <h4>{beer.tagline}</h4>
-              <p>
-                <b>Created by: </b> {beer.contributed_by}{" "}
-              </p>
-
-              <hr />
-            </div>
+        {beersList
+          .filter((beer) =>
+            searchTerm.length > 0
+              ? beer.name
+                  .toLocaleLowerCase()
+                  .includes(searchTerm.toLocaleLowerCase())
+              : beer
           )
-        })}
+          .map((beer) => {
+            return (
+              <div key={beer._id}>
+                <img
+                  src={beer.image_url}
+                  style={{ width: "50px" }}
+                  alt={beer.name}
+                />
+                <Link
+                  to={`/beers/${beer._id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <h2>{beer.name}</h2>
+                </Link>
+                <br />
+                <h4>{beer.tagline}</h4>
+                <p>
+                  <b>Created by: </b> {beer.contributed_by}{" "}
+                </p>
+
+                <hr />
+              </div>
+            )
+          })}
       </div>
     </div>
   )
--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,10 +1,99 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import Header from "../components/Header"
 
 const NewBeer = () => {
+  const defaultFormData = {
+    name: "",
+    tagline: "",
+    description: "",
+    first_brewed: "",
+    brewers_tips: "",
+    attenuation_level: 0,
+    contributed_by: "",
+  }
+
+  const [formData, setFormData] = useState(defaultFormData)
+  const navigateTo = useNavigate()
+
+  const addNewBeer = async () => {
+    const { data } = await axios.post(
+      `https://ih-beers-api2.herokuapp.com/beers/new`,
+      formData
+    )
+    navigateTo("/beers")
+  }
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    try {
+      addNewBeer()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div>
       <Header />
-      <h1>New Beer</h1>
+      <h1>Add a new beer</h1>
+
+      <form onSubmit={submitHandler}>
+        <label>Name: </label>
+        <input
+          type="text"
+          name="name"
+          value={formData.title}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>Tagline: </label>
+        <input
+          type="text"
+          name="tagline"
+          value={formData.tagline}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>Description: </label>
+        <textarea
+          type="text"
+          name="description"
+          value={formData.description}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>First Brewed: </label>
+        <input
+          type="text"
+          name="first_brewed"
+          value={formData.first_brewed}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>Attenuation Level: </label>
+        <input
+          type="number"
+          name="attenuation_level"
+          value={formData.attenuation_level}
+          onChange={changeHandler}
+        />
+        <br />
+        <label>Contributed by: </label>
+        <input
+          type="text"
+          name="contributed_by"
+          value={formData.contributed_by}
+          onChange={changeHandler}
+        />
+        <br />
+        <button type="submit">Brew!</button>
+      </form>
     </div>
   )
 }
